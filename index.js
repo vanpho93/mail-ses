fc39d5c2c574c5ff347339dd3d454ccce3c1331e
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-22 16:37:20 +0700

    send email

---
+++ index.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+
+const ACCESSS_KEY_ID = '**************';
+const SECRET_ACCESS_KEY = '************';
+
+AWS.config.update({
+    accessKeyId: ACCESSS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+    region: 'us-east-1'
+});
+
+const ses = new AWS.SES({ ses: '2010-12-01' });
+
+var params = {
+    Destination: { ToAddresses: ['vanpho02@gmail.com'] },
+    Message: {
+        Body: { Text: { Data: 'Xin chao quy vi va cac ban.' } },
+        Subject: { Data: 'Test send mail' }
+    },
+    ReturnPath: 'vanpho01@gmail.com',
+    Source: 'vanpho01@gmail.com'
+};
+
+ses.sendEmail(params, function (err, data) {
+    if (err) return console.log(err, err.stack);
+    console.log(data);
+});
